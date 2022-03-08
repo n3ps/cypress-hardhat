ab3f16585aded7828f522e47f3209f7a38000b14
Alan Snyder <alan.snyder@coinbase.com>
2022-03-08 17:29:13 -0500

    Implement CB Wallet user name lookups, and invoke read-contract for crypto punks via ethers

--- src/components/FetchUser.jsx
+++ src/components/FetchUser.jsx
@@ -1,15 +1,42 @@
-function FetchUser() {
-  const handleClick = () => {
-    alert('todo')
-  }
+import { requestCBWalletUser } from '../blockchain'
+import { useState } from 'react'
 
+function FetchUser() {
+  const [user, setUser] = useState('')
+  const [error, setError] = useState()
+
+  const handleClick = async () => {
+    const { user, error } = await requestCBWalletUser()
+
+    if (user) {
+      setUser(user)
+      setError('')
+    } else if (error) {
+      setUser('')
+      setError(error)
+    }
+  }
   return (
     <div className="flex gap-4">
-      <button className="btn btn-primary" onClick={handleClick}>
-        Fetch user
+      <button
+        data-testid="btn"
+        className="btn btn-primary"
+        onClick={handleClick}
+      >
+        Get User
       </button>
 
-      <p>Result goes here</p>
+      {error && (
+        <p data-testid="error" className="text-red-500 font-bold">
+          {error}
+        </p>
+      )}
+
+      {user && (
+        <div>
+          <p className="text-green-700 font-bold">User: {user}</p>
+        </div>
+      )}
     </div>
   )
 }
