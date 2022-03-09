752fd6be894ffa31b909361e4de83c1b82bd66ca
Vadim Brodsky <vadim.brodsky@coinbase.com>
2022-03-09 15:04:14 -0500

    Pass hardcoded input for now

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,17 @@ function App() {
 
       <GetAccount />
 
-      <p>Step 2: Programatically fetch user</p>
+      <p>Step 2: Programmatically fetch user</p>
 
-      <FetchUser />
+      <FetchUser
+        input={{
+          address: '0x5c728354230f7db92350b7616f45791dfe286279',
+          message:
+            'Hi from Coinbase NFT. Sign this message to prove you have access to this wallet and we’ll log you in. This won’t cost you any Ether. Timestamp: 1646842820',
+          signature:
+            '0x6771e3542032f9f4a3d179edf902abd4db9d1333d0e28af8ab0198f6c17dffc371922844a6a6a6945bb0137093fb0faf6aafc75be8b0ea0d38338607f95dbf351b',
+        }}
+      />
 
       <p>Step 3: Call web3 contract</p>
 
