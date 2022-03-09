ff3db993dfab5dc5161fc1dc825b73492e9fa088
Vadim Brodsky <vadim.brodsky@coinbase.com>
2022-03-09 15:04:03 -0500

    Add fetch call to get the user information from mapi

--- src/components/FetchUser.jsx
+++ src/components/FetchUser.jsx
@@ -1,7 +1,30 @@
-function FetchUser() {
+import { useState } from 'react'
+
+const API_URL = 'http://localhost:3000/api/graphql'
+
+function FetchUser({ input }) {
+  const queryId = '2109e710a2f12eac0a7deecb1da46ca3'
+  const [data, setData] = useState(null)
+  const [error, setError] = useState()
+
   const handleClick = () => {
-    alert('todo')
+    fetch(API_URL, {
+      headers: {
+        accept: 'application/json',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        'x-query-id': queryId,
+      },
+      body: JSON.stringify({ variables: { input } }),
+      method: 'POST',
+      mode: 'cors',
+    })
+      .then((res) => res.json())
+      .then(setData)
+      .catch(setError)
   }
+
+  console.log(data)
 
   return (
     <div className="flex gap-4">
@@ -9,7 +32,28 @@ function FetchUser() {
         Fetch user
       </button>
 
-      <p>Result goes here</p>
+      {error && (
+        <p data-testid="error" className="text-red-500 font-bold">
+          {error}
+        </p>
+      )}
+
+      {data && (
+        <div>
+          <p className="text-green-700 font-bold">
+            Id:&nbsp;
+            <span data-testid="result">
+              {data.data.findOrCreateUser.user.id}
+            </span>
+          </p>
+          <p className="text-green-700 font-bold">
+            Username:&nbsp;
+            <span data-testid="result">
+              {data.data.findOrCreateUser.user.profile.username}
+            </span>
+          </p>
+        </div>
+      )}
     </div>
   )
 }
